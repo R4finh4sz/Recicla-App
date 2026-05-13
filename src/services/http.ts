ac35d44c425afc9a19575c916c858736ad557338
Rafael Souza Santana <131507059+R4finh4sz@users.noreply.github.com>
2026-05-13 19:56:26 -0300

    Update baseURL

--- src/services/http.ts
+++ src/services/http.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
 const http = axios.create({
-  baseURL: 'https://reciclaonline.com.br/api',
+  baseURL: 'https://coral-app-xzr8d.ondigitalocean.app/api',
 });
 
 const maskSensitiveFields = (value: unknown): unknown => {
